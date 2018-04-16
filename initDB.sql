9c0e3b3d33d9076d01ad10965d102ec596d58583
m.fomichov <m.fomichov@gmail.com>
2018-04-16 17:05:54 +0300

    add ON DELETE CASCADE ON UPDATE CASCADE

--- initDB.sql
+++ initDB.sql
@@ -38,24 +38,24 @@ founder varchar(200));
 -- создаём таблицу связи many to many - developers_projects
 CREATE TABLE developers_projects (developer_id int, project_id int,
 primary key (developer_id, project_id),
-CONSTRAINT fk_id_developer_project FOREIGN KEY (developer_id) REFERENCES developers (id),
-CONSTRAINT fk_id_project_developer FOREIGN KEY (project_id) REFERENCES projects (id));
+CONSTRAINT fk_id_developer_project FOREIGN KEY (developer_id) REFERENCES developers (id) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_id_project_developer FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE);
 
 -- создаём таблицу связи many to many - developers_skills
 CREATE TABLE developers_skills (developer_id int, skill_id int,
 primary key (developer_id, skill_id),
-CONSTRAINT fk_id_developer_skill FOREIGN KEY (developer_id) REFERENCES developers (id),
-CONSTRAINT fk_id_skill_developer FOREIGN KEY (skill_id) REFERENCES skills (id));
+CONSTRAINT fk_id_developer_skill FOREIGN KEY (developer_id) REFERENCES developers (id) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_id_skill_developer FOREIGN KEY (skill_id) REFERENCES skills (id) ON DELETE CASCADE ON UPDATE CASCADE);
 
 -- создаём таблицу связи many to many - companies_projects
 CREATE TABLE companies_projects (company_id int, project_id int,
-CONSTRAINT fk_id_company_project FOREIGN KEY (company_id) REFERENCES companies (id),
-CONSTRAINT fk_id_project_company FOREIGN KEY (project_id) REFERENCES projects (id));
+CONSTRAINT fk_id_company_project FOREIGN KEY (company_id) REFERENCES companies (id) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_id_project_company FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE);
 
 -- создаём таблицу связи many to many - customers_projects
 CREATE TABLE customers_projects (customer_id int, project_id int,
-CONSTRAINT fk_id_customer_project FOREIGN KEY (customer_id) REFERENCES customers (id),
-CONSTRAINT fk_id_project_customer FOREIGN KEY (project_id) REFERENCES projects (id));
+CONSTRAINT fk_id_customer_project FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_id_project_customer FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE);
 
 
 
